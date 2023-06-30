b15910e4b7985051824bc490cf9fa83684a2089a
y1u0d2a1i <y1u0d2a1i@icloud.com>
2023-06-30 14:38:54 +0200

    update

--- mlptools/io/parser.py
+++ mlptools/io/parser.py
@@ -3,6 +3,8 @@ import numpy as np
 
 from abc import ABC, abstractmethod
 from ase.io import read
+from ase.io.espresso import read_espresso_out, read_espresso_in
+
 
 from mlptools.utils.utils import get_param_idx, remove_empty_from_array
 
@@ -34,6 +36,10 @@ class BaseParser(ABC):
     @abstractmethod
     def get_total_magnetization(self):
         raise NotImplementedError()
+    
+    @abstractmethod
+    def get_ase_atoms(self):
+        raise NotImplementedError()
 
 
 # Quantum espresso
@@ -42,6 +48,13 @@ class PWscfParser(BaseParser):
         super().__init__()
         self.path_to_target = path_to_target
         self.name_scf_in = name_scf_in
+
+        with open(os.path.join(path_to_target, name_scf_out)) as f:
+            atom_gen = read_espresso_out(f, index=slice(None))
+            ase_atoms = next(atom_gen)
+            print(ase_atoms)
+        self.ase_atoms = ase_atoms
+
         with open(f'{path_to_target}/{name_scf_in}') as f:
             self.I_lines = [s.strip() for s in f.readlines()]
         with open(f'{path_to_target}/{name_scf_out}') as f:
@@ -49,18 +62,9 @@ class PWscfParser(BaseParser):
         
         self.validate_o_lines()
         
-        num_atom = self.I_lines[get_param_idx('nat', self.I_lines)]
-        num_atom = num_atom.split(' ')[-1]
-        self.num_atom = int(num_atom)
-        
-        cell_idx = get_param_idx('CELL_PARAMETERS', self.I_lines)
-        self.cell = self.I_lines[cell_idx+1 : cell_idx+4]
-        
-        coord_idx = get_param_idx('ATOMIC_POSITIONS', self.I_lines)
-        self.coord = self.I_lines[coord_idx+1 : coord_idx+1+self.num_atom]
-        
-        self.au2ang = self.get_au2ang()
-        self.rv2ev = 13.60
+        self.num_atom = ase_atoms.get_global_number_of_atoms()
+        self.cell = np.array([vec for vec in ase_atoms.cell])
+        self.coord = ase_atoms.positions
 
         mpid = list(filter(lambda x: 'mp-' in x, path_to_target.split('/')))
         if structure_id is None and len(mpid) > 0:
@@ -72,33 +76,19 @@ class PWscfParser(BaseParser):
 
     
     def get_cell(self):
-        cell_matlix = self.cell.copy()
-        cell_matlix = np.array([list(map(lambda x: float(x), remove_empty_from_array(l.split(' ')))) for l in cell_matlix])
-        return cell_matlix
+        return self.cell
     
 
     def get_coord(self):
-        structure = read(os.path.join(self.path_to_target, self.name_scf_in), format='espresso-in')
-        return structure.get_positions()
+        return self.coord
 
 
     def get_energy(self, is_ev=True):
-        energy_idx = get_param_idx('!', self.O_lines)
-        energy = float(list(filter(lambda l: l != '', self.O_lines[energy_idx].split(' ')))[-2])
-        if is_ev:
-            return energy * self.rv2ev
-        else:
-            return energy
+        return self.ase_atoms.get_potential_energy()
         
             
     def get_force(self, is_ev_ang=True):
-        force_idx = get_param_idx('Forces acting on atoms (cartesian axes, Ry/au):', self.O_lines)  
-        start = force_idx+2
-        end = force_idx+2 + self.num_atom
-        forces = [list(filter(lambda l: l != '', line.split(' ')))[-3:] for line in self.O_lines[start:end]]
-        if is_ev_ang:
-            forces = [ np.array([float(i) for i in force]) * (self.rv2ev / self.au2ang) for force in forces]
-        return np.array(forces)
+        return self.ase_atoms.get_forces()
     
 
     def get_structure_id(self):
@@ -107,6 +97,9 @@ class PWscfParser(BaseParser):
 
     def get_n_atoms(self):
         return self.num_atom
+    
+    def get_ase_atoms(self):
+        return self.ase_atoms
     
     def get_total_magnetization(self):
         total_mag = list(filter(lambda x: 'total magnetization' in x, self.O_lines))
